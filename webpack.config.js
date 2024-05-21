a48794dbb0d1e57f33789596ddacf488d3446036
Dany Kopel <dany.kopel@tipalti.com>
2024-05-21 14:36:09 +0300

    Applied Webpack file copy via copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const WebpackCopyPlugin = require('webpack-copy-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: path.resolve(__dirname, 'src/js/index.js'),
@@ -12,15 +12,12 @@ module.exports = {
         static: path.resolve(__dirname, 'dist')
     },
     plugins: [
-        new WebpackCopyPlugin({
-            dirs: [{
-                from: path.resolve(__dirname, 'src/index.html'),
-                to: 'index.html',
-            }
-            ]
-        }),
-        new WebpackCopyPlugin({
-            dirs: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/index.html'),
+                    to: 'index.html',
+                },
                 {
                     from: path.resolve(__dirname, 'src/css/styles.css'),
                     to: 'styles.css'
